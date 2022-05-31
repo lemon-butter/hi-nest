f57a44cae5ffb094758c4ca49258e3bd936ca201
Junginet <kim1231401@naver.com>
2022-05-31 17:58:16 +0900

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 //데코레이터 -> 클레스에 함수기능을 추가 할 수 있음
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [MoviesController],
+  providers: [MoviesService],
 })
 
 //앱모듈은 비어있지만 진짜는 모듈(데코레이터)에 있음
---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Patch,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  // @Get('search')
+  // search(@Query('year') searchingYear: string) {
+  //   return `We are searching for a movie made after: ${searchingYear}`;
+  // }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: string) {
+    return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updateMovie: movieId,
+      ...updateData,
+    };
+  }
+}
